3ef27dd98b9b17f1df0938527205b77c09afbfe7
luong <dragonstone289@gmail.com>
2025-03-31 09:15:43 +0700

    Fix base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "static",
+    base: "/LuongPortpolio/",
+    site: "https://hoangluong1112.github.io/LuongPortpolio/",
+});
